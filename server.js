163398c74671f472319b57a82cd200daac7d56a5
shanreed <srtech5set@gmail.com>
2019-06-07 08:56:37 -0700

    added routers endpoints to server file

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+
+const actionsRouter = require('./data/actions/actionsRouter');
+const projectsRouter = require('./data/projects/projectsRouter');
 
 const server = express();
 
 // Middleware
 server.use(express.json());
 server.use(morgan("dev"));
+
+//Routes
+server.use('api/actions', actionsRouter);
+server.use('api/projects', projectsRouter);
 
 
 
