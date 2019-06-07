c91c51004b8b006a298613cfced2e7d872c2e737
shanreed <srtech5set@gmail.com>
2019-06-07 09:14:43 -0700

    added actions and projects routers

---
+++ data/actions/actionsRouter.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const actionsDb = require('../helpers/actionModel');
+
+
+
+const router = require('express').Router();
+
+module.exports = router;
---
+++ data/projects/projectsRouter.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const projectsDb = require('../helpers/projectModel');
+
+
+const router = require('express').Router();
+
+module.exports = router;
